26e6e4cb0e7435e5127ec470fec4ff8a7a5dbd7e
TheZombrex <davidfernandezperez.thezombrex@gmail.com>
2023-04-14 13:15:18 +0200

    Añadido el portfolio

---
+++ portfolio/js/index.js
@@ -0,0 +1,10 @@
+window.sr = ScrollReveal();
+
+    sr.reveal('.transition_section', {
+        duration: 2000
+    });
+
+sr.reveal('.delay_transition1');
+sr.reveal('.delay_transition2', { delay: 500 });
+sr.reveal('.delay_transition3', { delay: 1000 });
+sr.reveal('.delay_transition4', { delay: 1500 });
