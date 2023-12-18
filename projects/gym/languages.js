e86e7f0c11804c8e0c6eeca096a1ecf16815b73d
TheZombrex <davidfernandezperez.thezombrex@gmail.com>
2023-12-18 13:41:47 +0100

    Nuevo proyecto

---
+++ projects/gym/languages.js
@@ -0,0 +1,82 @@
+var languages = {
+    "idioma_selector": {
+      "castellano": "Español",
+      "english": "English"
+    },
+    "inicio": {
+      "castellano": "INICIO",
+      "english": "HOME"
+    },
+    "porque-elegirnos-menu": {
+      "castellano": "¿POR QUE ELEGIRNOS?",
+      "english": "WHY CHOOSE US?"
+    },
+    "porque-elegirnos-titulo": {
+      "castellano": "¿POR QUE ELEGIRNOS?",
+      "english": "WHY CHOOSE US?"
+    },
+    "entrenadoras": {
+      "castellano": "ENTRENADORAS",
+      "english": "COACHES"
+    },
+    "contacto-menu": {
+      "castellano": "CONTACTO",
+      "english": "CONTACT"
+    },
+    "contacto-boton": {
+      "castellano": "CONTACTO",
+      "english": "CONTACT"
+    },
+    "contacto-titulo": {
+      "castellano": "CONTACTO",
+      "english": "CONTACT"
+    },
+    "equipamiento-calidad": {
+      "castellano": "EQUIPAMIENTO DE CALIDAD",
+      "english": "QUALITY EQUIPMENT"
+    },
+    "nutricion": {
+      "castellano": "NUTRICIÓN",
+      "english": "NUTRITION"
+    },
+    "plan-dieta": {
+      "castellano": "PLAN DE DIETA SANO",
+      "english": "HEALTHY DIET PLAN"
+    },
+    "entrenamiento": {
+      "castellano": "ENTRENAMIENTO",
+      "english": "SPORT TRAINING"
+    },
+    "mente-sana": {
+      "castellano": "MENTE SANA, CUERPO SANO",
+      "english": "HEALTHY MIND, HEALTHY BODY"
+    },
+    "leer-mas": {
+      "castellano": "LEER MÁS",
+      "english": "READ MORE"
+    },
+    "nuestras-entrenadoras": {
+      "castellano": "NUESTRAS ENTRENADORAS",
+      "english": "OUR COACHES"
+    },
+    "in-nombre": {
+      "castellano": "Nombre",
+      "english": "Name"
+    },
+    "in-telefono": {
+      "castellano": "Teléfono",
+      "english": "Phone"
+    },
+    "message": {
+      "castellano": "Mensaje",
+      "english": "Message"
+    },
+    "in-enviar": {
+      "castellano": "ENVIAR",
+      "english": "SEND"
+    },
+    "ubicacion": {
+      "castellano": "Ubicación",
+      "english": "Location"
+    }
+  }
